2a468eb54318a5d7be4f0562ae7e40c917cb9dac
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2022-01-04 23:45:33 +0900

    Add setting and variables

--- UniswapV2/safeTransfer.sol
+++ UniswapV2/safeTransfer.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.0;
 
 import "https://github.com/SungJunEun/Solidity_Practice/blob/main/UniswapV2/UniswapV2ERC20_0.8.0.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/SungJunEun/Solidity_Practice/blob/main/UniswapV2/libraries/UQ112x112.sol";
 
 contract TOKEN is ERC20 {
     constructor() ERC20("MOYED", "MYD") {
@@ -11,9 +12,62 @@ contract TOKEN is ERC20 {
 }
 
 contract MyContract {
+    using UQ112x112 for uint224;
+
+    uint public constant MINIMUM_LIQUIDITY = 10**3;
     bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
 
-    function safeTransfer(address token, address to, uint value) public  {
+    address public factory;
+    address public token0;
+    address public token1;
+
+    uint112 private reserve0;           // uses single storage slot, accessible via getReserves
+    uint112 private reserve1;           // uses single storage slot, accessible via getReserves
+    uint32  private blockTimestampLast; // uses single storage slot, accessible via getReserves
+
+    uint public price0CumulativeLast;
+    uint public price1CumulativeLast;
+    uint public kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
+
+    uint private unlocked = 1;
+    modifier lock() {
+        require(unlocked == 1, 'UniswapV2: LOCKED');
+        unlocked = 0;
+        _;
+        unlocked = 1;
+    }
+
+    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
+        _reserve0 = reserve0;
+        _reserve1 = reserve1;
+        _blockTimestampLast = blockTimestampLast;
+    }
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+        constructor() {
+        factory = msg.sender;
+    }
+
+    // called once by the factory at time of deployment
+    function initialize(address _token0, address _token1) external {
+        require(msg.sender == factory, 'UniswapV2: FORBIDDEN'); // sufficient check
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    function _safeTransfer(address token, address to, uint value) private  {
         
         // Equivalent to ERC20(token).transfer(to,value);
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
